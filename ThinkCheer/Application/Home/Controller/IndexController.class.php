8357c6694892a2c59e32efff4b2281e696393b14
wuyiwen0 <1317281881@qq.com>
2018-03-30 21:12:24 +0800

    前台CSS、js、img引入成功

--- ThinkCheer/Application/Home/Controller/IndexController.class.php
+++ ThinkCheer/Application/Home/Controller/IndexController.class.php
@@ -2,17 +2,17 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-	public function __construct() {
-		parent::__construct();
-		if (!isLogin()) {
-			$this->redirect("Signup/signin");
-		}
-	}
+	// public function __construct() {
+	// 	parent::__construct();
+	// 	if (!isLogin()) {
+	// 		$this->redirect("Signup/signin");
+	// 	}
+	// }
     public function index(){
-    	$Model = M();
-    	$article = $Model->table("article a,user b")->where("a.authorid = b.id")->limit(10)->select();
+    	// $Model = M();
+    	// $article = $Model->table("article a,user b")->where("a.authorid = b.id")->limit(10)->select();
     	// dump($article);
-    	$this->assign("article", $article);
+    	// $this->assign("article", $article);
         $this->display();
     }
 }
